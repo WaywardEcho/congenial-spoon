6266e3fe03259474efeafa9a96753ae2b1baee14
mmeganmcnamara <113136952+mmeganmcnamara@users.noreply.github.com>
2025-03-24 13:08:41 -0400

    megan's ip

--- Chatsite/src/app/ChatClient.java
+++ Chatsite/src/app/ChatClient.java
@@ -108,7 +108,7 @@ public class ChatClient {
     }
 
     public static void main(String[] args) {
-    	String serverAddress = "192.168.39.250"; //Change this to the ip_address of the computer running the server
+    	String serverAddress = "10.140.232.53"; //Change this to the ip_address of the computer running the server
         int serverPort = 8080;
         new ChatClient(serverAddress, serverPort); //localhost/server. 
     }
